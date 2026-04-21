1406beeabadda38ca7c6659c9cd1841613acddcb
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-04-21 11:29:47 -0300

    primeiros passos do estudo do React

--- src/App.tsx
+++ src/App.tsx
@@ -1,121 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import './styles/theme.css'
+import './styles/globals.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <h1>Olá Mundo!</h1>
+      <p>
+        Lorem ipsum dolor sit amet. Ut enim voluptatem non optio quae sit
+        aliquid nobis id reprehenderit quis ut dolorum voluptatem et neque modi
+        nam rerum quasi. Ut quas quos sit cumque esse quo sunt doloremque et
+        aliquid dolorem ex perspiciatis laudantium non aliquam harum?{' '}
+      </p>
+      <p>
+        Nam doloremque quis sed exercitationem cumque quo eveniet distinctio a
+        nisi repudiandae ut atque adipisci ea possimus esse. Est cupiditate
+        distinctio At animi dolorem quo unde consectetur et voluptas cupiditate
+        qui commodi iste. Et aspernatur voluptate est vero aliquid ad amet
+        molestiae ut velit reprehenderit. Est nisi nobis sit fugit amet ex rerum
+        delectus.{' '}
+      </p>
+      <p>
+        Sit voluptas provident a quis doloribus et nihil itaque. Est
+        perspiciatis tempore est doloremque necessitatibus est eaque eveniet.
+        Est fugit laboriosam et internos fuga quo corrupti sunt aut ratione
+        unde.{' '}
+      </p>
     </>
-  )
+  );
 }
 
-export default App
