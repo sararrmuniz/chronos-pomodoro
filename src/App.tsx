023ad02f1a22b821ff7867718292b291543beb3c
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-04-23 16:30:00 -0300

    home chronos

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,54 @@ import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/Countdown';
+import { DefaultInput } from './components/DefaultInput';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
+import { PlayCircleIcon } from 'lucide-react';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
     <>
       <Container>
-        <Logo/>
+        <Logo />
       </Container>
 
       <Container>
-        <Menu/>
+        <Menu />
       </Container>
 
       <Container>
-        <CountDown/>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form className='form' action=''>
+          <div className='formRow'>
+            <DefaultInput
+              labelText='task'
+              id='meuInput'
+              type='text'
+              placeholder='Digite algo'
+            />
+          </div>
+
+          <div className='formRow'>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+
+          <div className='formRow'>
+            <Cycles></Cycles>
+          </div>
+
+          <div className='formRow'>
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
+          </div>
+        </form>
+      </Container>
+
+      <Container>
+        <Footer></Footer>
       </Container>
     </>
   );
