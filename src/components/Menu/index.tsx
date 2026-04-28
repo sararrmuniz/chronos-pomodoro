693c0cb72b36b1da230907939bb00a1af894a70b
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-04-28 15:27:13 -0300

    theme change

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,21 +1,60 @@
 import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
 
-export function Menu () {
-    return (
-        <nav className={styles.menu}>
-            <a href='#' className={styles.menuLink}>
-               <HouseIcon/>
-            </a>
-            <a href='#' className={styles.menuLink}>
-               <HistoryIcon/>
-            </a>
-            <a href='#' className={styles.menuLink}>
-               <SettingsIcon/>
-            </a>
-            <a href='#' className={styles.menuLink}>
-               <SunIcon/>
-            </a>
-        </nav>
-    )
+type AvailableThemes = 'dark' | 'light';
+
+export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+    //document.documentElement.setAttribute('data-theme', theme);
+  }
+
+  return (
+    <nav className={styles.menu}>
+      <h1>{theme}</h1>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ir para a Home'
+        title='Ir para Home'
+      >
+        <HouseIcon />
+      </a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
+        <HistoryIcon />
+      </a>
+      <a
+        href='#'
+        className={styles.menuLink}
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+      <a
+        href='#'
+        onClick={handleThemeChange}
+        className={styles.menuLink}
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+      >
+        <SunIcon />
+      </a>
+    </nav>
+  );
 }
