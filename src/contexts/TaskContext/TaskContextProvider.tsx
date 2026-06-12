7ca66873e72666e0fe53fb2e75cb85509d72e1a7
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-06-12 14:17:10 -0300

    fazendo o ádio funcionar no final do timer

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -11,6 +12,7 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   useEffect(() => {
     const worker = TimerWorkerManager.getInstance();
@@ -19,6 +21,10 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
       const countDownSeconds = e.data as number;
 
       if (countDownSeconds <= 0) {
+        if (playBeepRef.current) {
+          playBeepRef.current();
+          playBeepRef.current = null;
+        }
         dispatch({
           type: TaskActionTypes.COMPLETE_TASK,
         });
@@ -43,6 +49,14 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
       worker.onmessage(() => {});
     };
   }, [state]);
+
+  useEffect(() => {
+    if (state.activeTask && playBeepRef.current === null) {
+      playBeepRef.current = loadBeep();
+    } else {
+      playBeepRef.current = null;
+    }
+  }, [state.activeTask]);
 
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
