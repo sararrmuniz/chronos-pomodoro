17b37673aeb02486d542216cf23b8cabdb904baa
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-06-15 11:31:19 -0300

    salvndo e recuperando o estado do localStorage

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -5,16 +5,33 @@ import { taskReducer } from './taskReducer';
 import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 import { TaskActionTypes } from './taskActions';
 import { loadBeep } from '../../utils/loadBeep';
+import type { TaskStateModel } from '../../models/TaskStateModel';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
-  const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+  const [state, dispatch] = useReducer(taskReducer, initialTaskState, () => {
+    const storageState = localStorage.getItem('state');
+
+    if (storageState === null) return initialTaskState;
+
+    const parsedStorageState = JSON.parse(storageState) as TaskStateModel;
+
+    return {
+      ...parsedStorageState,
+      activeTask: null,
+      secondsRemaining: 0,
+      formattedSecondsRemaining: '00:00',
+    };
+  });
+  
   const playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
   useEffect(() => {
+    localStorage.setItem('state', JSON.stringify(state));
+    
     const worker = TimerWorkerManager.getInstance();
 
     const handleMessage = (e: MessageEvent) => {
@@ -38,6 +55,8 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
     };
 
     worker.onmessage(handleMessage);
+
+    document.title = `${state.formattedSecondsRemaining} - Chronos Pomodoro`;
 
     if (!state.activeTask) {
       worker.terminate();
