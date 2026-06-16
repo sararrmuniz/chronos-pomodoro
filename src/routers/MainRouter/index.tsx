c030973dd53d33c49ea01752a189b2036dea879d
Sara R R Muniz <sararrmuniz@hotmail.com>
2026-06-16 15:51:36 -0300

    Desenvolvendo e validando campos do formulário da página Settings

--- src/routers/MainRouter/index.tsx
+++ src/routers/MainRouter/index.tsx
@@ -4,6 +4,7 @@ import { NotFound } from "../../pages/NotFound";
 import { Home } from "../../pages/Home";
 import { History } from "../../pages/History";
 import { useEffect } from "react";
+import { Settings } from "../../pages/Settings";
 
 function ScrollToTop(){
     const { pathname } = useLocation();
@@ -20,6 +21,7 @@ export function MainRouter() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/history/' element={<History />} />
+            <Route path='/settings/' element={<Settings />} />
             <Route path='/about-pomodoro/' element={<AboutPomodoro />} />
 
             <Route path='*' element={<NotFound />} />
